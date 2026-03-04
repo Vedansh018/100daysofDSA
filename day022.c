daceeaf3cec1d959421f4db645a9c7be44c6f065
Vedansh018 <vedu0429@gmail.com>
2026-03-04 23:44:45 +0530

    Add files via upload

---
+++ day022.c
@@ -0,0 +1,65 @@
+//Problem: Count Nodes in Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    
+    return count;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, value;
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+    struct Node* newNode = NULL;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        newNode = createNode(value);
+
+        if(head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    printList(head);
+    
+    int total = countNodes(head);
+    printf("\nTotal Nodes: %d", total);
+
+    return 0;
+}
