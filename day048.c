ef0facfe75e61a68a6a743d465ac71af65a881aa
Vedansh018 <vedu0429@gmail.com>
2026-03-27 22:08:18 +0530

    Add files via upload

---
+++ day048.c
@@ -0,0 +1,36 @@
+//Problem: Count Leaf Nodes
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+int countLeaves(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("%d", countLeaves(root));
+    return 0;
+}
