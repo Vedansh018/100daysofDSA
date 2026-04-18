17c35e189b8c6e1304f792fe145209c232463107
Vedansh018 <vedu0429@gmail.com>
2026-04-18 22:00:23 +0530

    Add files via upload

---
+++ day067.c
@@ -0,0 +1,58 @@
+//Problem: Print topological ordering of a Directed Acyclic Graph (DAG) using DFS.
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+int stack[MAX];
+int top = -1;
+int n;
+
+void push(int v) {
+    stack[++top] = v;
+}
+
+void dfs(int v) {
+    visited[v] = 1;
+
+    for(int i = 0; i < n; i++) {
+        if(adj[v][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+
+    push(v);   // push after visiting all neighbors
+}
+
+void topologicalSort() {
+    for(int i = 0; i < n; i++) {
+        if(!visited[i])
+            dfs(i);
+    }
+
+    printf("Topological Order:\n");
+    while(top != -1) {
+        printf("%d ", stack[top--]);
+    }
+}
+
+int main() {
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    topologicalSort();
+
+    return 0;
+}
